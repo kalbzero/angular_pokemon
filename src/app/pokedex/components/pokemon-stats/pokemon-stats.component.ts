ef3f879f2385cb320b523d00959107aede9fe0e7
Edward Ramos <edward.ramos@procer.com.br>
2025-11-15 18:37:45 -0300

    feat: stats colorbar done

--- src/app/pokedex/components/pokemon-stats/pokemon-stats.component.ts
+++ src/app/pokedex/components/pokemon-stats/pokemon-stats.component.ts
@@ -10,4 +10,17 @@ import { TitleCasePipe } from '@angular/common';
 })
 export class PokemonStatsComponent {
   @Input() pokemon: IPokemon | undefined;
+  private maxBaseStat = 255; // maior valor possível na Pokédex
+
+  public getStatPercent(stat: number): number {
+    return Math.round((stat / this.maxBaseStat) * 100);
+  }
+
+  public getStatColor(stat: number): string {
+    const percent = this.getStatPercent(stat);
+
+    if (percent < 30) return 'var(--stat-bad)'; // fraco
+    if (percent < 60) return 'var(--stat-medium)'; // ok
+    return 'var(--stat-good)'; // forte
+  }
 }
